37238ada95e136d04389d9a45ac5ecb8adaabb81
Tabitha Goodall <tg.twitchett@gmail.com>
2018-02-25 20:30:09 +0100

    moving circles around

--- src/lines/core.cljs
+++ src/lines/core.cljs
@@ -3,34 +3,48 @@
             [quil.middleware :as m]))
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
-  (q/frame-rate 30)
+  (q/frame-rate 10)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
-  {:color 0
-   :angle 0})
+  (let [start (list (q/random 500) (q/random 500))
+        end   (list (q/random 500 (q/random 500)))]
+    {:color 0
+     :size 10
+     :cur 0
+     :xc (first start)
+     :yc (second start)
+     :x2 (first end)
+     :y2 (second end)}))
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  {:color 122
+   :size (:size state)
+   :cur (inc (:cur state))
+   :xc (q/lerp (:xc state) (:x2 state) 0.05)
+   :yc (q/lerp (:yc state) (:y2 state) 0.05)
+   :x2 (:x2 state)
+   :y2 (:y2 state)})
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
+    (q/ellipse (:xc state) (:yc state) 20 20)
+    (q/fill 127 255 255)
+    (q/ellipse (:x2 state) (:y2 state) 20 20)
+    (q/fill 210 255 255))
+
   ; Set circle color.
-  (q/fill (:color state) 255 255)
+  ; (q/fill (:color state) 255 255)
   ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  ; (let [angle (:angle state)
+  ;       x (* 150 (q/cos angle))
+  ;       y (* 150 (q/sin angle))]
+  ;   ; Move origin point to the center of the sketch.
+  ;   (q/with-translation [(/ (q/width) 2)
+  ;                        (/ (q/height) 2)]
+  ;     ; Draw the circle.
+  ;     (q/ellipse x y 100 100))))
 
 ; this function is called in index.html
 (defn ^:export run-sketch []
@@ -43,9 +57,7 @@
     :update update-state
     :draw draw-state
     ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
     :middleware [m/fun-mode]))
 
 ; uncomment this line to reset the sketch:
-; (run-sketch)
+(run-sketch)
