de7b81bdd0c8df1bf7ecfade820ea02b64eb962e
Tabitha Goodall <tg.twitchett@gmail.com>
2018-02-26 21:37:16 +0100

    converged

--- src/lines/core.cljs
+++ src/lines/core.cljs
@@ -3,7 +3,7 @@
             [quil.middleware :as m]))
 
 (defn setup []
-  (println "well then")
+  (println "-- setup")
   (q/frame-rate 10)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
@@ -12,46 +12,37 @@
   (let [start (list (q/random 500) (q/random 500))
         end   (list (q/random 500) (q/random 500))]
     {:color 0
-     :xc (first start)
-     :yc (second start)
+     :x (first start)
+     :y (second start)
      :x2 (first end)
      :y2 (second end)}))
 
 (defn abs [n] (max n (- n)))
 
-(defn converged [i j]
-  (< 5 (q/random 10))
+(defn converged? [a b]
+  (println "abs is " (abs (- a b)))
+  (< 0.1 (abs (- a b))))
 
-(defn target-reached [state]
-  (println "araaargh")
-  (println state)
-  (and (converged (:xc state) (:x2 state))
-       (converged (:yc state) (:y2 state))))
+(defn update-state [{:keys [x x2 y y2] :as state}]
+  (println "-- update " state)
+  (if (converged? x x2)
+    {:color 122
+      :x (q/lerp x x2 0.05)
+      :y (q/lerp y y2 0.05)
+      :x2 x2
+      :y2 y2}
+    {:color 0
+     :x 0
+     :y 0
+     :x2 250
+     :y2 250}
+    ))
 
-(defn update-state [state]
-  (println state)
-  ; Update sketch state by changing circle color and position.
-  (if (target-reached state)
-    ; set a new target
-    ((let [start (list (q/random 500) (q/random 500))
-           end   (list (q/random 500) (q/random 500))]
-      {:color 0
-       :xc (first start)
-       :yc (second start)
-       :x2 (first end)
-       :y2 (second end)}))
-    ; move the shape towards the target
-    ({:color 122
-      :xc (q/lerp (:xc state) (:x2 state) 0.05)
-      :yc (q/lerp (:yc state) (:y2 state) 0.05)
-      :x2 (:x2 state)
-      :y2 (:y2 state)})))
-
-(defn draw-state [state]
-    (q/ellipse (:xc state) (:yc state) 20 20)
-    (q/fill 127 255 255)
-    (q/ellipse (:x2 state) (:y2 state) 20 20)
-    (q/fill 210 255 255))
+(defn draw-state [{:keys [x x2 y y2] :as state}]
+  (q/ellipse x y 50 50)
+  (q/fill 127 255 255)
+  (q/ellipse x2 y2 20 20)
+  (q/fill 100 100 100))
 
   ; Set circle color.
   ; (q/fill (:color state) 255 255)
@@ -79,4 +70,4 @@
     :middleware [m/fun-mode]))
 
 ; uncomment this line to reset the sketch:
-(run-sketch)
+; (run-sketch)
