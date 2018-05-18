157a71d50e7a7011b264de33c0f2e3ae0ab30f5e
Tabitha Goodall <tg.twitchett@gmail.com>
2018-05-18 18:10:09 +0200

    chore: cleanup

--- resources/public/js/development/figwheel/connect/build_development.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-development (:require [lines.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "development", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
